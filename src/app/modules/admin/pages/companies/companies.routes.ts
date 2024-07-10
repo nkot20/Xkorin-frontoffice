e13ab19a6bd476098a0b919746ebad5bbbd22658
nkot20 <74417927+nkot20@users.noreply.github.com>
2024-07-10 16:29:43 +0100

    evolution graphic

--- src/app/modules/admin/pages/companies/companies.routes.ts
+++ src/app/modules/admin/pages/companies/companies.routes.ts
@@ -6,6 +6,7 @@ import {ExamService} from "../../../../core/exam/exam.service";
 import {catchError, throwError} from "rxjs";
 import {CompanyService} from "../../../../core/company/company.service";
 import {ListComponent} from "./list/list.component";
+import {ImprintService} from "../../../../core/imprint/imprint.service";
 
 const companiesResolver = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
     const companyService =  inject(CompanyService);
@@ -29,12 +30,42 @@ const companiesResolver = (route: ActivatedRouteSnapshot, state: RouterStateSnap
     );
 };
 
+const examImprintsCompanieDetailsResolver = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+    const imprintService =  inject(ImprintService);
+    const router = inject(Router);
+    const id = route.paramMap.get('id');
+
+    return imprintService.getImprintsValuesDetailsCompanies("6675cc8228464981b4cbc539", id).pipe(
+        // Error here means the requested category is not available
+        catchError((error) => {
+            // Log the error
+            console.error(error);
+
+            // Get the parent url
+            const parentUrl = state.url.split('/').slice(0, -1).join('/');
+
+            // Navigate to there
+            router.navigateByUrl(parentUrl);
+
+            // Throw an error
+            return throwError(error);
+        }),
+    );
+};
+
 export default [
     {
         path     : '',
         component: ListComponent,
         resolve: {
             companies: companiesResolver
+        }
+    },
+    {
+        path     : 'details/:id',
+        component: ListComponent,
+        resolve: {
+            details: examImprintsCompanieDetailsResolver
         }
     },
 ] as Routes;
--- src/app/modules/admin/pages/companies/details/details.component.ts
+++ src/app/modules/admin/pages/companies/details/details.component.ts
@@ -1,10 +1,169 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {Observable} from "rxjs";
+import {environment} from "../../../../../../environments/environment";
+import {ImprintService} from "../../../../../core/imprint/imprint.service";
+import {ExamService} from "../../../../../core/exam/exam.service";
+import {Chart} from "chart.js";
+import {MatProgressBarModule} from "@angular/material/progress-bar";
+import {CommonModule, NgClass, NgForOf} from "@angular/common";
+import {MatButtonModule} from "@angular/material/button";
+import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import {MatIconModule} from "@angular/material/icon";
+import {MatMenuModule} from "@angular/material/menu";
+import {NgApexchartsModule} from "ng-apexcharts";
+import {MatTabsModule} from "@angular/material/tabs";
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
+    standalone: true,
+    imports: [
+        MatProgressBarModule,
+        NgClass,
+        NgForOf,
+        CommonModule,
+        MatButtonModule,
+        MatButtonToggleModule,
+        MatIconModule,
+        MatMenuModule,
+        NgApexchartsModule,
+        MatTabsModule
+    ]
 })
-export class DetailsComponent {
+export class DetailsComponent implements AfterViewInit, OnInit {
 
+    imprints$: Observable<any[]>;
+    imprints: any[];
+    examDetails$: Observable<any>;
+    examScore$: Observable<number>;
+    imprintStats$: Observable<any[]>;
+    imprintsValues: number[];
+    imprintStats: any[];
+    // inclusive confidence index
+    cci: number;
+    categories = [
+        {
+            name: 'Personal relationship',
+            statuses: ['green star', 'green star', 'green star', 'green star', 'green star', 'green star', 'green star']
+        },
+        {name: 'Stay in family', statuses: ['green', 'green', 'green', 'gray', 'gray', 'green star', 'green star']},
+        {
+            name: 'Member of a local association',
+            statuses: ['green', 'green', 'green', 'gray', 'gray', 'green star', 'green star']
+        },
+        {
+            name: 'Member of an association',
+            statuses: ['green star', 'green star', 'green star', 'green star', 'gray', 'green star', 'green star']
+        },
+        {
+            name: 'Contribute to the well-being',
+            statuses: ['red', 'red', 'red', 'gray', 'gray', 'green star', 'green star']
+        }
+    ];
+
+
+    progress = 80;
+    urlCertificat: string = environment.apiFile + '/certificats/imprints-fusion/';
+    examId: string;
+
+    constructor(private _imprintService: ImprintService, private _examService: ExamService) {
+    }
+
+    ngOnInit() {
+        this.urlCertificat = this.urlCertificat + this._examService.idExam + ".pdf";
+        this.imprints$ = this._imprintService.imprints$;
+        this.imprints$.subscribe(value => {
+            this.imprints = value;
+        })
+        this.examDetails$ = this._imprintService.examDetails$;
+        //this.examScore$ = this._imprintService.indexScore$;
+        this.imprintStats$ = this._imprintService.imprintStatistics$;
+        this.imprintStats$.subscribe(value => {
+            this.imprintStats = value;
+        });
+        this._imprintService.imprintsValues$.subscribe(value => {
+            this.imprintsValues = value.reverse();
+            this.cci = value.reduce((sum, value) => sum + value, 0);
+        })
+    }
+
+    ngAfterViewInit() {
+        this.renderChart(this.imprintStats);
+    }
+
+    renderChart(datas: any[]) {
+        const ctx = document.getElementById('assessmentChart') as HTMLCanvasElement;
+        let min = [];
+        let max = [];
+        let moy = [];
+        let current = [];
+        let labels = [];
+        datas.forEach(value => {
+            labels.push(value.imprint);
+            min.push(value.minValue);
+            max.push(value.maxValue);
+            moy.push(value.averageValue);
+        })
+        new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: labels,
+                datasets: [
+                    {
+                        label: 'Min',
+                        data: min,
+                        borderColor: 'red',
+                        fill: false,
+                    },
+                    {
+                        label: 'Moy',
+                        data: moy,
+                        borderColor: 'black',
+                        borderDash: [5, 5],
+                        fill: false,
+                    },
+                    {
+                        label: 'Max',
+                        data: max,
+                        borderColor: 'green',
+                        fill: false,
+                    },
+                    {
+                        label: 'Current',
+                        data: this.imprintsValues,
+                        borderColor: 'orange',
+                        pointRadius: 5,
+                        pointBackgroundColor: 'orange',
+                        fill: false,
+                    }
+                ]
+            },
+            options: {
+                responsive: true,
+                animations: {
+                    tension: {
+                        duration: 1000,
+                        easing: 'linear',
+                        from: 1,
+                        to: 0,
+                        loop: true
+                    }
+                },
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    }
+
+    toArray(value) {
+        return Array(value).fill(0)
+    }
+
+    indexIsAvalaible() {
+        return this.imprints.every(value => value.isAvailable);
+    }
 }
